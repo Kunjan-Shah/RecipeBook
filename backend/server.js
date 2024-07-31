e71768ed123eddff9b844e5858f341a7f987f148
Kunjan-Shah <kunjan.nshah1@gmail.com>
2024-07-31 10:34:10 +0530

    Add frontend origin access

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,12 @@ const recipeRouter = require("./routes/recipe");
 
 require("./mongo-connect")
 
+const corsOptions = {
+    "origin": "https://recipebook-kmj4.onrender.com"
+}
+
 const PORT = process.env.PORT || 8000;
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({limit: '10mb'}));
 
